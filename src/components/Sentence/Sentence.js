218705708830d422b81ac8a93f2b98050c2fe4eb
Tim Bridge <timothy.bridge@newellco.com>
2019-04-30 14:48:21 -0400

    Started initalizing source ddata objects.

--- src/components/Sentence/Sentence.js
+++ src/components/Sentence/Sentence.js
@@ -4,6 +4,21 @@ import Chord from "../../components/Chord/Chord.js";
 import RomanNumeral from "../../components/RomanNumeral/RomanNumeral.js";
 import Key from "../../components/Key/Key.js";
 
+const A = {
+    I : "A",
+    II : "Bm",
+    III : "C#m",
+    IV : "D",
+    V : "E",
+    VI : "F#m",
+    VII : "G#m",
+}
+
+const majorKeys = {
+    A : A,
+};
+
+
 class Sentence extends Component {
     renderChord(i) {
         return <Chord />;
@@ -16,12 +31,13 @@ class Sentence extends Component {
     renderKey(i) {
         return <Key />;
     }
-
     
 
     render(i) {
         return (
             <div>
+                { Object.keys(majorKeys.A) }
+                <br/>
                 <Chord value={i}/> is the <RomanNumeral value={i}/> in the key of <Key value={i}/>
             </div>
         );
