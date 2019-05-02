e1a89c7736b07b5f63c4763db115b5ffd770efbd
Tim Bridge <timothy.bridge@newellco.com>
2019-05-02 15:10:00 -0400

    Used arrays for key definitions.

--- src/components/Sentence/Sentence.js
+++ src/components/Sentence/Sentence.js
@@ -5,15 +5,78 @@ import RomanNumeral from "../../components/RomanNumeral/RomanNumeral.js";
 import Key from "../../components/Key/Key.js";
 
 const A = {
-    chords : {
-        I: "A",
-            II: "Bm",
-            III: "C#m",
-            IV: "D",
-            V: "E",
-            VI: "F#m",
-            VII: "G#dim"
-    },
+    chords : [
+        {  // A
+            nashvilleRoman : 'I',
+            nashvilleArabic : '1',
+            chordAbbreviation : "A",
+            chordFull : "A major",
+            chordQuality : "major",
+            chordQualityAbbreviation : "",
+            baseNote : 'A',
+            accidental: "",
+        },
+        {  // Bm
+            nashvilleRoman : 'II',
+            nashvilleArabic : '2',
+            chordAbbreviation: "Bm",
+            chordFull : "B minor",
+            chordQuality : "minor",
+            chordQualityAbbreviation : "m",
+            baseNote : 'B',
+            accidental: "",
+        },
+        {  // C#m
+            nashvilleRoman : 'III',
+            nashvilleArabic : '3',
+            chordFull: "C# minor",
+            chordAbbreviation : "C#m",
+            chordQuality : "minor",
+            chordQualityAbbreviation : "m",
+            baseNote : 'C',
+            accidental: "#",
+        },
+        {  // D
+            nashvilleRoman : 'IV',
+            nashvilleArabic : '4',
+            chordFull: "D major",
+            chordAbbreviation : "D",
+            chordQuality : "major",
+            chordQualityAbbreviation : "",
+            baseNote : 'D',
+            accidental: "",
+        },
+        {  // E
+            nashvilleRoman : 'V',
+            nashvilleArabic : '5',
+            chordFull: "E major",
+            chordAbbreviation : "E",
+            chordQuality : "major",
+            chordQualityAbbreviation : "",
+            baseNote : 'E',
+            accidental: "",
+        },
+        {  // F#m
+            nashvilleRoman : 'VI',
+            nashvilleArabic : '6',
+            chordFull: "F# minor",
+            chordAbbreviation : "F#m",
+            chordQuality : "minor",
+            chordQualityAbbreviation : "m",
+            baseNote : 'F',
+            accidental: "#",
+        },
+        {  // G#dim
+            nashvilleRoman : 'VII',
+            nashvilleArabic : '7',
+            chordFull: "G# diminished",
+            chordAbbreviation : "G#°",
+            chordQualityFull : "diminished",
+            chordQualityAbbreviation : "°",
+            baseNote : 'G',
+            accidental: "#",
+        },
+    ],
     randomChord : function () {
         let objectKeys = Object.keys(this.chords);
         console.log(objectKeys);
@@ -24,20 +87,80 @@ const A = {
 };
 
 const B = {
-    chords : {
-        I: "B",
-        II: "C#m",
-        III: "D#m",
-        IV: "E",
-        V: "F#",
-        VI: "G#m",
-        VII: "A#dim"
-    },
+    chords : [
+        {  // B
+            nashvilleRoman : 'I',
+            nashvilleArabic : '1',
+            chordAbbreviation : "B",
+            chordFull : "B major",
+            chordQuality : "major",
+            chordQualityAbbreviation : "",
+            baseNote : 'B',
+            accidental: "",
+        },
+        {  // C#m
+            nashvilleRoman : 'II',
+            nashvilleArabic : '2',
+            chordAbbreviation: "C#m",
+            chordFull : "C# minor",
+            chordQuality : "minor",
+            chordQualityAbbreviation : "m",
+            baseNote : 'C',
+            accidental: "#",
+        },
+        {  // D#m
+            nashvilleRoman : 'III',
+            nashvilleArabic : '3',
+            chordFull: "D# minor",
+            chordAbbreviation : "D#m",
+            chordQuality : "minor",
+            chordQualityAbbreviation : "m",
+            baseNote : 'D',
+            accidental: "#",
+        },
+        {  // E
+            nashvilleRoman : 'IV',
+            nashvilleArabic : '4',
+            chordFull: "E major",
+            chordAbbreviation : "E",
+            chordQuality : "major",
+            chordQualityAbbreviation : "",
+            baseNote : 'E',
+            accidental: "",
+        },
+        {  // F#
+            nashvilleRoman : 'V',
+            nashvilleArabic : '5',
+            chordFull: "F# major",
+            chordAbbreviation : "F#",
+            chordQuality : "major",
+            chordQualityAbbreviation : "",
+            baseNote : 'F',
+            accidental: "#",
+        },
+        {  // G#m
+            nashvilleRoman : 'VI',
+            nashvilleArabic : '6',
+            chordFull: "G# minor",
+            chordAbbreviation : "G#m",
+            chordQuality : "minor",
+            chordQualityAbbreviation : "m",
+            baseNote : 'G',
+            accidental: "#",
+        },
+        {  // A#dim
+            nashvilleRoman : 'VII',
+            nashvilleArabic : '7',
+            chordFull: "A# diminished",
+            chordAbbreviation : "A#°",
+            chordQualityFull : "diminished",
+            chordQualityAbbreviation : "°",
+            baseNote : 'A',
+            accidental: "#",
+        },
+    ],
     randomChord : function () {
         let objectKeys = Object.keys(this.chords);
-        console.log(objectKeys);
-        console.log(objectKeys[objectKeys.length * Math.random() << 0]);
-        //console.log(this.keys[objectKeys[objectKeys.length * Math.random() << 0]]);
         return this.chords[objectKeys[objectKeys.length * Math.random() << 0]];
     }
 };
@@ -49,9 +172,6 @@ const majorKeys = {
     },
     randomKey: function () {
         let objectKeys = Object.keys(this.keys);
-        console.log(objectKeys);
-        console.log(objectKeys[objectKeys.length * Math.random() << 0]);
-        //console.log(this.keys[objectKeys[objectKeys.length * Math.random() << 0]]);
         return this.keys[objectKeys[objectKeys.length * Math.random() << 0]];
     }
 };
@@ -68,32 +188,18 @@ class Sentence extends Component {
             currentKey : newKey,
             currentChord : newKey.randomChord(),
         }
-        console.log(this.state.currentKey);
         // End state
     }
     // End Constructor
 
-/*    renderChord(key) {
-        return <Chord />;
-    }
-
-    renderRomanNumeral(key) {
-        return <RomanNumeral />;
-    }
-
-    renderKey(i) {
-       // let { majorKeys } = this.state;
-        return <Key value={i}/>;
-    }*/
-
-    
-
-    render(i) {
+    render() {
+        let { currentKey } = this.state;
+        let { currentChord } = this.state;
         return (
             <div>
               {/* { Object.keys(majorKeys.keys.A) }*/}
                 <br/>
-                <Chord value={this.state.currentChord}/> is the <RomanNumeral value={Object.keys(this.state.currentChord).toString()}/> in the key of <Key value={this.state.currentKey.chords.I} />
+                <Chord value={currentChord.chordAbbreviation}/> is the <RomanNumeral value={currentChord.nashvilleRoman}/> in the key of <Key value={currentKey.chords[0].chordAbbreviation} />
             </div>
         );
     }
