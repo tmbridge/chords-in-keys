1d834e0bd1777020a2163d310e55fa7da5be658e
Tim Bridge <timothy.bridge@newellco.com>
2019-05-10 16:47:12 -0400

    Moved un-random related functions to utils.js

--- src/lib/helpers/random.js
+++ src/lib/helpers/random.js
@@ -12,22 +12,3 @@ export const getRandomKey = (currentKeys) => {
     let length = objectKeys.length;
     return currentKeys[objectKeys[random(length)]];
 }
-
-// TODO: write/test this method
-/*export const getKeyByAbbreviation = (abbreviatedName) => {
-    currentKeys.chords.find(abbreviatedName);
-}*/
-
-// TODO: write/test this method
-/*export const getMatchingRandomChord = (quality,
-                                       baseNote,
-                                       accidental,) => {
-    let allKeys = {};
-    if (quality === "major") {
-        let allKeys = this.allKeyGroupsWithFilteredKeys.major;
-    }
-    let objectKeys = Object.keys(allKeys);
-    return allKeys.chords[objectKeys[objectKeys.length * Math.random() << 0]];
-}*/
-
-// TODO: write a function that will replace unicode accidental characters ('♭','♯','♮') with ASCII characters ('b', '#', '') to use in systems unable to parse unicode?
